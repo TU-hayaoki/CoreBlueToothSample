da719b86f2abdba7fea68997d7e5b8a205c50cf0
TU-hayaoki <156896546+TU-hayaoki@users.noreply.github.com>
2024-10-08 07:26:41 +0900

    Initial Commit

---
+++ CoreBlueToothSample/CoreBlueToothSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreBlueToothSampleApp.swift
+//  CoreBlueToothSample
+//
+//  Created by Hayato Aoki on 2024/10/08.
+//
+
+import SwiftUI
+
+@main
+struct CoreBlueToothSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
